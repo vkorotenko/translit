1fb9931e2b486fc19facf324794ce5c41ace65c9
Vladimir Korotenko <koroten@ya.ru>
2015-11-05 17:57:44 +0400

    no brother release

---
+++ Kvn.Translit/UaEngParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kvn.Translit
+{
+    public class UaEngParser
+    {
+        public string Transliterate(string source)
+        {
+            return "swidomitie pishite samis";
+        }
+    }
+}
